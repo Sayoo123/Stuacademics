f83592cbdb7e9c546e1e0ab4d9d27e7068ef42f5
Sayoo123 <sayoojsunil2000@gmail.com>
2023-08-30 15:46:37 +0530

    login and register successful

--- app/src/main/java/com/example/stuacademics/Login.java
+++ app/src/main/java/com/example/stuacademics/Login.java
@@ -1,26 +1,95 @@
 package com.example.stuacademics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class Login extends AppCompatActivity {
-
+EditText email,password;
+ProgressBar bar;
+Button logintomain;
+FirebaseAuth mAuth;
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            startActivity(new Intent(Login.this, MainActivity.class));
+//        }
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         TextView reg=findViewById(R.id.Signup);
+        email=findViewById(R.id.Loginemail);
+        password=findViewById(R.id.Loginpassword);
+        bar=findViewById(R.id.progress_bar);
+        logintomain=findViewById(R.id.Login);
+        mAuth=FirebaseAuth.getInstance();
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, Registeration.class));
+            }
+        });
+        logintomain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bar.setVisibility(View.VISIBLE);
+                String emailauth,passwordauth;
+                emailauth=String.valueOf(email.getText());
+                passwordauth=String.valueOf(password.getText());
+
+                if(TextUtils.isEmpty(emailauth))
+                {
+                    Toast.makeText(Login.this,"Fill Email field",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(passwordauth))
+                {
+                    Toast.makeText(Login.this,"Fill Password field",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(emailauth, passwordauth)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                bar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this, "LOGGED IN",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(Login.this, "LOGIN FAILED",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
     }
--- app/src/main/java/com/example/stuacademics/Registeration.java
+++ app/src/main/java/com/example/stuacademics/Registeration.java
@@ -1,16 +1,93 @@
 package com.example.stuacademics;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registeration extends AppCompatActivity {
-
+    EditText email,password,confirm_password;
+    Button register;
+    ProgressBar bar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration);
+        email=findViewById(R.id.Register_email);
+        password=findViewById(R.id.Register_password);
+        confirm_password=findViewById(R.id.Register_confirmpassword);
+        register=findViewById(R.id.Register);
+        mAuth= FirebaseAuth.getInstance();
+        bar=findViewById(R.id.progress_bar);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        TextView tologin=findViewById(R.id.backtologin);
+        tologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registeration.this, Login.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bar.setVisibility(View.VISIBLE);
+                String emailauth,passwordauth,cr_password_auth;
+                emailauth=String.valueOf(email.getText());
+                passwordauth=String.valueOf(password.getText());
+                cr_password_auth=String.valueOf(confirm_password.getText());
+                System.out.println(emailauth+passwordauth+cr_password_auth);
+                if(TextUtils.isEmpty(emailauth))
+                {
+                    Toast.makeText(Registeration.this,"Fill Email field",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(passwordauth))
+                {
+                    Toast.makeText(Registeration.this,"Fill Password field",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!(passwordauth.equals(cr_password_auth)))
+                {
+                    Toast.makeText(Registeration.this,"Password mismatch",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(emailauth, passwordauth)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                bar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Registeration.this, "Registered Successfully.",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Registeration.this, Login.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Registeration.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 }
